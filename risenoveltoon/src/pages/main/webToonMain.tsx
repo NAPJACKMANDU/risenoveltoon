94780102e6c4de14e10a68a1f8e2bc11385c8b18
yunseonam <sjw070283828@gmail.com>
2026-07-27 16:02:14 +0900

    수정

--- risenoveltoon/src/pages/main/webToonMain.tsx
+++ risenoveltoon/src/pages/main/webToonMain.tsx
@@ -6,6 +6,7 @@ import {useState} from "react";
 import {WebToonMainTabList} from "../main/webToonMainTabList.tsx"
 import {novelToonData} from "../../components/mainToon/webToonData.tsx"
 
+
 export const MainHome = () => {
     const categoryTitle = [
         { id: "all", title: "📋 전체" },
